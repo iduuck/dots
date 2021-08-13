27a3057d18cfe43480e198106bf067849b6ec469
Nick Schmidt <nick@kreativgebiet.com>
2021-08-13 13:27:03 +0200

    load asdf properly and hide "missing command" errors

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -2,13 +2,13 @@
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
 # Try loading ASDF from the regular home dir location
-#if [ -f "$HOME/.asdf/asdf.sh" ]; then
-#  . "$HOME/.asdf/asdf.sh"
-#elif which brew >/dev/null &&
-#  BREW_DIR="$(dirname `which brew`)/.." &&
-#  [ -f "$BREW_DIR/opt/asdf/asdf.sh" ]; then
-#  . "$BREW_DIR/opt/asdf/asdf.sh"
-#fi
+if [ -f "$HOME/.asdf/asdf.sh" ]; then
+ . "$HOME/.asdf/asdf.sh"
+elif which brew >/dev/null &&
+ BREW_DIR="$(dirname `which brew`)/.." &&
+ [ -f "$BREW_DIR/opt/asdf/asdf.sh" ]; then
+ . "$BREW_DIR/opt/asdf/asdf.sh"
+fi
 
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
