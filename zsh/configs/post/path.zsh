23aee0aae9760ce8b4abdb4b1eafffdc8371f8fa
Nick Schmidt <nick@kreativgebiet.com>
2021-08-26 13:03:49 +0200

    add ability to disable PATH modification with npm or yarn

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -13,4 +13,19 @@ fi
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
+if type "npm" > /dev/null && [ "$FDF_ZSH_CONFIG_PATH_DISABLE_NPM" != true ]; then
+  PATH="$(npm bin -g):$PATH"
+
+  # We don't need the path twice, hence we are disabling this for yarn here.
+  # However, you can force enable this when you export the variable with a value
+  # of `false`.
+  if [ "$FDF_ZSH_CONFIG_PATH_DISABLE_YARN" == false ]; then
+    export FDF_ZSH_CONFIG_PATH_DISABLE_YARN=true
+  fi
+fi
+
+if type "yarn" > /dev/null && [ "$FDF_ZSH_CONFIG_PATH_DISABLE_YARN" != true ]; then
+  PATH="$(yarn global bin):$PATH"
+fi
+
 export -U PATH
