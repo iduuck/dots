d1e9fafb2b042691399ba490356882514617d890
Nick Schmidt <nick@kreativgebiet.com>
2021-08-31 15:40:38 +0200

    add snippets to vim

--- config/nvim/lua/fintory/lsp.lua
+++ config/nvim/lua/fintory/lsp.lua
@@ -6,8 +6,14 @@ local cmp = require'cmp'
 cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
+    { name = 'luasnip' },
     { name = 'buffer' },
     { name = 'path' },
+  },
+  snippet = {
+    expand = function(args)
+      require'luasnip'.lsp_expand(args.body)
+    end
   },
   mapping = {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
@@ -15,7 +21,7 @@ cmp.setup {
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
+      behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     })
   }
@@ -24,8 +30,10 @@ cmp.setup {
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
--- Setup: LSP
+-- Setup: luasnip
+require("luasnip/loaders/from_vscode").lazy_load {}
 
+-- Setup: LSP
 local lsp_on_attach = function(client)
   -- Mappings.
   local key_map = vim.api.nvim_buf_set_keymap
