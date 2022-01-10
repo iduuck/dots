ecac9920200cf2178a94ef48ab7df3f9c1c94bb4
Nick Schmidt <325241+iDuuck@users.noreply.github.com>
2022-01-10 20:14:56 +0100

    adjust spacing and change theme for lualine

--- config/nvim/lua/fintory/lualine.lua
+++ config/nvim/lua/fintory/lualine.lua
@@ -1,30 +1,31 @@
 require('lualine').setup({
-    options = {
-        component_separators = '',
-        section_separators = '',
-        icons_enabled = true,
+  options = {
+    theme = "github",
+    component_separators = '',
+    section_separators = '',
+    icons_enabled = true,
+  },
+  sections = {
+    lualine_a = {
+      { 'mode', color = { gui = 'bold' } },
     },
-    sections = {
-        lualine_a = {
-            { 'mode', color = { gui = 'bold' } },
-        },
-        lualine_b = {
-            { 'branch' },
-            { 'diff', colored = false },
-        },
-        lualine_c = {
-            { 'filename', file_status = true },
-            { 'diagnostics' },
-        },
-        lualine_x = {
-            'lsp_progress',
-            'filetype',
-            'encoding',
-            'fileformat',
-        },
-        lualine_y = { 'progress' },
-        lualine_z = {
-            { 'location', color = { gui = 'bold' } },
-        },
+    lualine_b = {
+      { 'branch' },
+      { 'diff', colored = false },
     },
+    lualine_c = {
+      { 'filename', file_status = true },
+      { 'diagnostics' },
+    },
+    lualine_x = {
+      'lsp_progress',
+      'filetype',
+      'encoding',
+      'fileformat',
+    },
+    lualine_y = { 'progress' },
+    lualine_z = {
+      { 'location', color = { gui = 'bold' } },
+    },
+  },
 })
