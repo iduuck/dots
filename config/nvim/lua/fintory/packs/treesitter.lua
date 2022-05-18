4a8c4fc0c0115d8476146864db7681e29171bf56
Nick Schmidt <325241+iDuuck@users.noreply.github.com>
2022-05-18 09:14:33 +0200

    added `treesitter` configuration to ensure installed packages

--- config/nvim/lua/fintory/packs/treesitter.lua
+++ config/nvim/lua/fintory/packs/treesitter.lua
@@ -1,3 +1,24 @@
+-- Configure treesitter to ensure some modules installed.
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+    "tsx",
+    "typescript",
+    "css",
+    "comment",
+    "bash",
+    "graphql",
+    "html",
+    "javascript",
+    "json",
+    "regex",
+    "ruby",
+    "scss",
+    "yaml"
+  },
+
+  sync_install = true,
+}
+
 -- Setup typescriptreact for treesitter
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
