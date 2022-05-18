25e778623af550a433843937ba80a8bdb2b443ab
Nick Schmidt <nick@fintory.com>
2022-05-18 09:19:30 +0200

    changed telescope configuration

--- config/nvim/lua/fintory/packs/telescope.lua
+++ config/nvim/lua/fintory/packs/telescope.lua
@@ -4,11 +4,37 @@ local K = require('fintory.keymap')
 -- used int he following keymaps
 _G.Telescope = setmetatable({}, {
     __index = function(_, k)
-        if vim.bo.filetype == 'NvimTree' then
-            A.nvim_command('wincmd l')
-        end
         return require('telescope.builtin')[k]
     end,
+})
+
+require('telescope').setup({
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+    },
+    prompt_prefix = "   ",
+    selection_caret = "  ",
+    entry_prefix = "  ",
+    file_sorter = require("telescope.sorters").get_fuzzy_file,
+    generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+    color_devicons = true,
+    set_env = { ["COLORTERM"] = "truecolor" },
+    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+    buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+    mappings = {
+      n = { ["q"] = require("telescope.actions").close },
+    },
+  }
 })
 
 K.n('<C-P>', '<CMD>lua Telescope.find_files()<CR>')
