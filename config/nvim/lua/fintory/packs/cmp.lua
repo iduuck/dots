c48d5e487760377507242cfeadfc7d2ffd89a84c
Nick Schmidt <nick@fintory.com>
2022-08-15 08:41:32 +0200

    feat(nvim): use up and down for cmp navigation

--- config/nvim/lua/fintory/packs/cmp.lua
+++ config/nvim/lua/fintory/packs/cmp.lua
@@ -9,6 +9,8 @@ cmp.setup({
     }),
   },
   mapping = cmp.mapping.preset.insert({
+    ['<Up>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+    ['<Down>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
