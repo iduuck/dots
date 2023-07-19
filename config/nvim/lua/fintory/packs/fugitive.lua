56591ebb46c5a07c222fb762377634effd4d98b1
Nick Schmidt <nick@fintory.com>
2023-07-19 09:41:09 +0200

    replace fugitive with neogit

--- config/nvim/lua/fintory/packs/fugitive.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "tpope/vim-fugitive",
-  config = function()
-    vim.keymap.set('n', '<leader>gs', '<cmd>Git<CR>')
-    vim.keymap.set('n', '<leader>gp', '<cmd>Git push<CR>')
-    vim.keymap.set('n', '<leader>gup', '<cmd>Git pull --rebase<CR>')
-  end
-}
